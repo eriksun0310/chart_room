76ee247970ef7414505d03e88075bc7ee616c80d
yu_dev <sun67737.vteam@gmail.com>
2024-02-02 17:54:14 +0800

    push 一半

---
+++ src/client/pages/chatRoom/index.ts
@@ -0,0 +1,21 @@
+import "./index.css";
+
+import { io } from 'socket.io-client'
+
+
+
+const url = new URL(location.href)
+const userName = url.searchParams.get('user_name')
+const roomName = url.searchParams.get('room_name')
+
+
+if(!userName || !roomName){
+    location.href = '/main/main.html'
+}
+
+// 1. 建立連接
+const clientIo = io()
+
+clientIo.on("join", (msg)=>{
+    console.log('msg', msg)
+})
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import devServer from "./server/dev";
+import prodServer from "./server/prod";
+import express from "express";
+import { Server } from 'socket.io'
+import http from 'http'
+
+import { name } from "@/utils";
+
+const port = 3008;
+const app = express();
+const server = http.createServer(app)
+const io = new Server(server)
+
+
+// 監測連接
+io.on('connection', (socket)=>{
+  socket.emit('join', 'welcome')
+})
+
+
+// 執行npm run dev本地開發 or 執行npm run start部署後啟動線上伺服器
+if (process.env.NODE_ENV === "development") {
+  devServer(app);
+} else {
+  prodServer(app);
+}
+
+
+
+console.log("server side", name);
+
+server.listen(port, () => {
+  console.log(`The application is running on port ${port}.`);
+});
