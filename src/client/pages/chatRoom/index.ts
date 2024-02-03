493a8269b7e827aa578cc745999d233ac6bd18e6
erika <erikawang1233@gmail.com>
2024-02-03 16:36:48 +0800

    chat room practice end

--- src/client/pages/chatRoom/index.ts
+++ src/client/pages/chatRoom/index.ts
@@ -1,13 +1,17 @@
 import "./index.css";
-
+import { UserData } from "@/service/UserService";
 import { io } from 'socket.io-client'
 
-
+type UserMsg = {
+    userData:UserData,
+    msg:string,
+    time:number
+}
 
 const url = new URL(location.href)
 const userName = url.searchParams.get('user_name')
 const roomName = url.searchParams.get('room_name')
-
+let userID = ''
 
 if(!userName || !roomName){
     location.href = '/main/main.html'
@@ -16,6 +20,99 @@ if(!userName || !roomName){
 // 1. 建立連接
 const clientIo = io()
 
+// 加入聊天室
+clientIo.emit("join", {userName, roomName})
+
+
+const textInput = document.getElementById('textInput') as HTMLInputElement
+const submitBtn = document.getElementById('submitBtn') as HTMLButtonElement
+const backBtn = document.getElementById('backBtn') as HTMLButtonElement
+const chatBoard = document.getElementById('chatBoard') as HTMLDivElement
+const headerRoomName = document.getElementById('headerRoomName') as HTMLParagraphElement
+headerRoomName.innerText = roomName || '-'
+
+function msgHandler(data:UserMsg){
+    const date = new Date(data.time)
+    const time = `${date.getHours()}:${date.getMinutes()}`
+    const divBox = document.createElement('div')
+    divBox.classList.add('flex','mb-4','items-end')
+    if(data.userData.id === userID){
+        divBox.classList.add('justify-end')
+        divBox.innerHTML = `
+        <p class="text-xs text-gray-700 mr-4">${time}</p>
+            <div>
+            <p class="text-xs text-white mb-1 text-right">${data.userData.userName}</p>
+            <p
+                class="mx-w-[50%] break-all bg-white px-4 py-2 rounded-bl-full rounded-br-full rounded-tl-full"
+            >
+                ${data.msg}
+            </p>
+        </div>
+        `
+    }else{
+        divBox.classList.add('justify-start')
+        divBox.innerHTML = `
+        <div>
+            <p class="text-xs text-gray-700 mb-1">${data.userData.userName}</p>
+            <p
+                class="mx-w-[50%] break-all bg-gray-800 px-4 py-2 rounded-tr-full rounded-br-full rounded-tl-full text-white"
+            >
+            ${data.msg}
+            </p>
+            </div>
+        <p class="text-xs text-gray-700 ml-4">${time}</p>
+        `
+    }
+
+    chatBoard.appendChild(divBox)
+    textInput.value = ''
+    chatBoard.scrollTop = chatBoard.scrollHeight
+}
+
+
+function roomMsgHandler(msg:string){
+    const divBox = document.createElement('div')
+    divBox.classList.add('flex', 'justify-center','mb-4','items-center')
+    divBox.innerHTML =`
+    <div class="flex justify-center mb-4 items-center">
+    <p class="text-gray-700 text-sm">${msg}</p>
+    </div>
+    `
+    chatBoard.appendChild(divBox)
+    chatBoard.scrollTop = chatBoard.scrollHeight
+}
+
+
+// 發送Btn
+submitBtn.addEventListener("click", ()=>{
+    const textValue = textInput.value
+    // console.log(textValue)
+    // 在chat 頻道 發送到後端
+    clientIo.emit("chat", textValue)
+})
+
+//返回Btn
+backBtn.addEventListener("click", ()=>{
+    location.href ="/main/main.html"
+})
+
+// 在聊天室 發送訊息
+clientIo.on('chat', (data:UserMsg)=>{
+    msgHandler(data)
+})
+
+
+
+// 加入聊天室
 clientIo.on("join", (msg)=>{
-    console.log('msg', msg)
+    roomMsgHandler(msg)
+})
+
+//離開聊天室
+clientIo.on('leave', (msg)=>{
+    roomMsgHandler(msg)
+})
+
+clientIo.on('userID', (id)=>{
+    userID = id
 })
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import devServer from "./server/dev";
-import prodServer from "./server/prod";
+import devServer from "@/server/dev";
+import prodServer from "@/server/prod";
 import express from "express";
 import { Server } from 'socket.io'
 import http from 'http'
+import UserService from "@/service/UserService";
+import moment from "moment";
 
 import { name } from "@/utils";
 
@@ -10,11 +12,57 @@ const port = 3008;
 const app = express();
 const server = http.createServer(app)
 const io = new Server(server)
+const userService = new UserService()
 
+
+// socket.on:用來接收訊息
+// socket.emit:用來傳遞訊息
 
 // 監測連接
 io.on('connection', (socket)=>{
-  socket.emit('join', 'welcome')
+
+  socket.emit("userID", socket.id)
+
+  socket.on('join', ({userName, roomName}:{userName:string, roomName:string})=>{
+    const userData = userService.userDataInfoHandler(
+      socket.id,
+      userName,
+      roomName
+    )
+
+    //用 roomName 創建一個唯一的空間
+    socket.join(userData?.roomName)
+
+    // 新增用戶 (socket.broadcast.to: 自己看不到自己發出的訊息
+    userService.addUser(userData)
+    socket.broadcast.to(userData.roomName).emit('join',`${userName}加入了${roomName}聊天室`)
+    // io.emit('join',`${userName}加入了${roomName}聊天室`)
+  })
+
+  // 後端收到從前端傳來的msg
+  socket.on('chat', (msg)=>{
+    const time  = moment.utc()
+    const userData = userService.getUser(socket.id)
+    if(userData){
+      // 發送給 特定的[roomName] 訊息( io.to:是指自己也要看到訊息
+      io.to(userData?.roomName).emit('chat', {userData, msg , time})
+      // io.emit('chat', msg)
+    }
+
+  })
+
+  // 斷開連結
+  socket.on('disconnect',()=>{
+    // 取得用戶
+    const userData = userService.getUser(socket.id)
+    const userName = userData?.userName
+    if(userName){
+      socket.broadcast.to(userData?.roomName).emit('leave', `${userName}離開聊天室`)
+      // io.emit('leave', `${userName}離開聊天室`)
+    }
+    // 移除用戶
+    userService.removeUser(socket.id)
+  })
 })
 
 
